d31796791d3c2bea0a9f8cd64c9a6e55026448b7
Sergey Neroda <nicebooyyy@gmail.com>
2020-10-19 15:50:28 +0300

    Add field password to Users table

--- app/User.php
+++ app/User.php
@@ -6,8 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'nickname','surname','avatar', 'phone', 'sex', 'show_phone',
+        'nickname','surname','avatar', 'phone', 'sex', 'show_phone', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
     ];
     
     /**
