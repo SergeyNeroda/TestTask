d9e83aa2fa3def23cd91e978e2577409fa3926be
Sergey Neroda <nicebooyyy@gmail.com>
2020-10-26 15:46:25 +0200

    Add image and redesign user profile page

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -5,33 +5,33 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Account Details Page') }}</div>
-
+                 <div class="card-header">{{ __('Деталі аккаунту') }}</div>
+                
                 <div class="card-body">
-                    <p>
-                        Nickname: {{$auth_user->nickname}}
-                    <p/>
-                    <p>
-                        Surname: {{$auth_user->surname}}
-                    <p/>
-                    <p>
-                        Avatar: {{$auth_user->avatar}}
-                    <p/>
-                    <p>
-                        Phone: {{$auth_user->phone}}
-                    <p/>
-                    <p>
-                        Sex: {{$auth_user->sex}}
-                    <p/>
-                    <p>
-                        Show Phone: {{$auth_user->show_phone}}
-                    <p/>
+                   
+                    <div class="card">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item ">Аватар: <img src="{{$auth_user->avatar}}" style="height:100px"/></li>
+                            <li class="list-group-item">Нікнейм: {{$auth_user->nickname}}</li>
+                            <li class="list-group-item">Прізвище: {{$auth_user->surname}}</li>
+                            <li class="list-group-item">Телефон: {{$auth_user->phone}}</li>
+                            <li class="list-group-item">Стать: {{$auth_user->sex}}</li>
+                            <li class="list-group-item">Показати телефон: {{$auth_user->show_phone}}</li>
+                          </ul>
+                    </div>
                     @if(session()->get('success'))
-                          <div class="alert alert-success">
-                              {{ session()->get('success') }}
-                          </div>
+                        <div class="alert alert-success">
+                            {{ session()->get('success') }}
+                        </div>
                     @endif
+                     
                 </div>
+
+                <div class="card-footer">
+                    <a href="{{ route('users.edit') }}" class="btn btn-primary">Редагувати</a>
+                    <a href="{{ route('users.edit_password') }}" class="btn btn-primary">Змінити пароль</a>
+                </div>
+                    
             </div>
 
         </div>
