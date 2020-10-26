83003fb183c8c50e3a5528480029ea477de9e10e
Sergey Neroda <nicebooyyy@gmail.com>
2020-10-26 15:44:55 +0200

    Add validation and image Service for register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+// Include Service for Image
+use App\Services\ImageService;
 
 class RegisterController extends Controller
 {
@@ -50,30 +52,78 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'nickname' => ['required', 'string', 'min:8', 'unique:users'],	
-            'surname' => ['required'],	
-            'avatar' =>	['required'],	
-            'phone'	=> ['required'],	
-            'sex' => ['required'],		
-            'show_phone' =>	['required'],	
-            'password' => ['required', 'string', 'min:8', 'confirmed'],	
-            'email' => ['required', 'string', 'email', 'max:255'],
+            'nickname' => [
+                'required', 
+                'string', 
+                'min:8', 
+                'unique:users', 
+                'alpha_dash'
+            ],	
+            'surname' => [
+                'required',
+                'string',
+                'alpha_dash'
+            ],	
+            'avatar' => [
+                'image',
+                'required',
+                'mimes:jpeg,png',
+                'dimensions:min_width=300,max_width=2000,min_height=300,max_height=2000',
+            ],
+            'phone'	=> [
+                'required',
+                'numeric'
+            ],	
+            'sex' => [
+                'required',
+                'in:Male,Female'
+            ],		
+            'show_phone' =>	[
+                'required',
+                'boolean'
+            ],	
+            'password' => [
+                'required', 
+                'string', 
+                'min:8', 
+                'confirmed'
+            ],	
+            'email' => [
+                'required',
+                'unique:users', 
+                'string', 
+                'email', 
+                'max:255'
+            ],
         ],[
             'nickname.required' => 'Поле Нікнейм обов\'язкове для заповнення',
             'nickname.min' => 'Поле Нікнейм має бути більше 8 символів',
             'nickname.unique' => 'Поле Нікнейм не унікальне',
+            'nickname.alpha_dash' => 'Нікнейм може містити лише літери, цифри, тире та підкреслення',
 
             'surname.required' => 'Поле Прізвище обов\'язкове для заповнення',
+            'surname.alpha_dash' => 'Прізвище може містити лише літери, цифри, тире та підкреслення',
 
             'avatar.required' => 'Поле Аватар обов\'язкове для заповнення',
+            'avatar.image' => 'Поле Аватар має бути картинкою формату jpeg або png' ,
+            'avatar.dimensions' => 'Зображення має бути не більше 2000х2000 пікселів та не меньше 300х300 пікселів' ,
 
             'phone.required' => 'Поле Телефон обов\'язкове для заповнення',
+            'phone.numeric' => 'Поле Телефон - лише числові символи',
 
             'sex.required' => 'Поле Стать обов\'язкове для заповнення',
+            'sex.in' => 'Поле Стать може мати значення Чоловік або Жінка',
 
             'show_phone.required' => 'Поле Показ телефону обов\'язкове для заповнення',
+            'show_phone.boolean' => 'Поле Показ телефону має бути істинним або хибним',
 
             'email.required' => 'Поле Імейл обов\'язкове для заповнення',
+            'email.unique' => 'Користувач з таким імейлом вже існує',
+
+            'password.required'=> 'Поле Пароль обов\'язкове для заповнення', 
+            'password.string'=> 'Поле Пароль повинне бути рядком', 
+            'password.min'=> 'Поле Пароль повинне мати мінімум 8 символів', 
+            'password.confirmed'=> 'Паролі не збігаються',
         ]);
     }
 
@@ -85,10 +135,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // Get file from request
+        $path = '/img/users/';
+        $avatar = (new ImageService())->uploadImage($data['avatar'],$data['nickname'], $path);
+
+        //dd($avatar);
         return User::create([
             'nickname' => $data['nickname'],	
             'surname' => $data['surname'],	
-            'avatar' =>	$data['avatar'],	
+            'avatar' =>	$avatar,	
             'phone'	=> $data['phone'],	
             'sex' => $data['sex'],		
             'show_phone' =>	$data['show_phone'],
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Реєстрація') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="nickname" class="col-md-4 col-form-label text-md-right">{{ __('Nickname') }}</label>
+                            <label for="nickname" class="col-md-4 col-form-label text-md-right">{{ __('Нікнейм') }}</label>
 
                             <div class="col-md-6">
                                 <input id="nickname" type="text" class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{ old('nickname') }}" required autocomplete="nickname" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Прізвище') }}</label>
 
                             <div class="col-md-6">
                                 <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname">
@@ -39,12 +39,13 @@
                             </div>
                         </div>
 
+
                         <div class="form-group row">
-                            <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Avatar') }}</label>
-
+                            <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Аватар') }}</label>
                             <div class="col-md-6">
-                                <input id="avatar" type="text" class="form-control @error('avatar') is-invalid @enderror" name="avatar" value="{{ old('avatar') }}" required autocomplete="avatar">
-
+                                
+                                <input id="avatar" class=" @error('avatar') is-invalid @enderror" value="{{ old('avatar') }}" required type="file"  name="avatar">
+                            
                                 @error('avatar')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -53,15 +54,8 @@
                             </div>
                         </div>
 
-                        {{--  <div class="form-group row">
-                            <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Avatar (optional)') }}</label>
-                            <div class="col-md-6">
-                                <input id="avatar" type="file" class="form-control" name="avatar">
-                            </div>
-                        </div>  --}}
-
                         <div class="form-group row">
-                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Телефон') }}</label>
 
                             <div class="col-md-6">
                                 <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
@@ -75,10 +69,14 @@
                         </div>
                         
                         <div class="form-group row">
-                            <label for="sex" class="col-md-4 col-form-label text-md-right">{{ __('Sex') }}</label>
+                            <label for="sex" class="col-md-4 col-form-label text-md-right">{{ __('Стать') }}</label>
 
                             <div class="col-md-6">
-                                <input id="sex" type="text" class="form-control @error('sex') is-invalid @enderror" name="sex" value="{{ old('sex') }}" required autocomplete="sex">
+                                <select id="sex" type="text" class="form-control @error('sex') is-invalid @enderror" name="sex"  required>
+                                    <option selected disabled>Оберіть стать...</option>
+                                    <option value="Male" @if (old('sex') == "Male") {{ 'selected' }} @endif>Чоловік</option>
+                                    <option value="Female" @if (old('sex') == "Female") {{ 'selected' }} @endif>Жінка</option>
+                                </select>
 
                                 @error('sex')
                                     <span class="invalid-feedback" role="alert">
@@ -89,21 +87,35 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="show_phone" class="col-md-4 col-form-label text-md-right">{{ __('Show Phone') }}</label>
+                            <label for="show_phone" class="col-md-4 col-form-label form-check-label text-md-right">{{ __('Показ телефону') }}</label>
 
                             <div class="col-md-6">
-                                <input id="show_phone" type="text" class="form-control @error('show_phone') is-invalid @enderror" name="show_phone" value="{{ old('show_phone') }}" required autocomplete="show_phone">
+                            {{--  <div class="form-control">  --}}
+                                <div class="form-check  @error('show_phone') is-invalid @enderror">
+                                    <input class="form-check-input" type="radio" name="show_phone" id="show_phone_1" value="0" @if(old('show_phone')=="0") checked @endif>
+                                    <label class="form-check-label" for="show_phone_1">
+                                      Приховати
+                                    </label>
+                                  </div>
 
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="show_phone" id="show_phone_2" value="1" @if(old('show_phone')=="1") checked @endif>
+                                    <label class="form-check-label" for="show_phone_2">
+                                      Показати
+                                    </label>
+                                  </div>
+                                
                                 @error('show_phone')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+                            {{--  </div>  --}}
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Імейл') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -117,7 +129,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -131,7 +143,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Підтвердити пароль') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -141,7 +153,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Реєстрація') }}
                                 </button>
                             </div>
                         </div>
