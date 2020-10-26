2487300b14d4e43c6530de0dd7681dbf34b58a72
Sergey Neroda <nicebooyyy@gmail.com>
2020-10-26 15:43:19 +0200

    Add Home page for unauthorized users

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index ()
+    {
+        $articles = Article::orderBy('created_at', 'desc')->get();
+        return view('home',['articles' => $articles]); 
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between align-items-center ">
+                        <h5 class="card-title mb-0">Усі статті</h5>
+                    </div>
+                </div>
+
+                <div class="card-body">
+
+                    @forelse ($articles as $article)
+                    <div class="card mb-3">
+                        <div class="card-header">
+                            <div>{{$article->title}}</div>
+                        </div>
+                        <div class="card-body">
+                            <div>{{$article->text}}</div>
+                        </div>
+                        {{--  <div class="card-footer">
+                            <div class="d-flex flex-row-reverse">
+                                  
+                            </div>
+                        </div>  --}}
+                    </div>
+
+                    @empty
+                        <div class="alert alert-warning">
+                            Статті відсутні
+                        </div>
+                    @endforelse
+
+                    
+                </div>
+               
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
