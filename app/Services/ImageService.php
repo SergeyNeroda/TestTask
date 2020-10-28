fb06402039d48fe4b0eaefa3aee20187652cc278
Sergey Neroda <nicebooyyy@gmail.com>
2020-10-28 13:51:23 +0200

    Error handing in ImageService

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -2,40 +2,45 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\File; 
 use Image;
 
 class ImageService {
     
-    //завантаження зображень 
+    /**
+     * Upload image
+     *
+     */
     public function uploadImage ($image, $name , $path)
     {
         $imagename = 'avatar-'.time().'-'.$name.'.'.$image->getClientOriginalExtension();
         $destinationPath = public_path($path);
-        //dd($destinationPath);
+        
         $processingImg = $this->processingImage($image);
         
-        $save = $this->saveImage($processingImg, $destinationPath, $imagename);
-       
-        if ($save) {
-            //link to image
-            return $path.$imagename;
-            //return $imagename;  
-        } else {
-            dd('error'); 
+        if (!$save = $this->saveImage($processingImg, $destinationPath, $imagename)) {
+            return abort('500');
         }
+        return $path.$imagename;
+
     }
 
-    //обробки зображень 
+    /**
+     * Processing image
+     *
+     */ 
     public function processingImage ($image)
     {
         $thumb_img = Image::make($image->getRealPath())->crop(300, 300);
         return $thumb_img;
     }
 
-    //збереження зображень
+    /**
+     * Save image
+     *
+     */
     public function saveImage ($image, $path, $name)
     {
-        $image->save($path.$name,80);
-        return true;       
+        return $image->save($path.$name,80);       
     }
 }
