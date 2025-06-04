47bc42d6f26c210f97f5fbad52976b43300f0fc3
Sergey <neroda.sergey1998@gmail.com>
2025-06-05 01:01:28 +0300

    Improve account page and layout

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -8,27 +8,25 @@
                  <div class="card-header">{{ __('Деталі аккаунту') }}</div>
                 
                 <div class="card-body">
-                   
-                    <div class="card">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item ">Аватар: <img src="{{$auth_user->avatar}}" style="height:100px"/></li>
-                            <li class="list-group-item">Нікнейм: {{$auth_user->nickname}}</li>
-                            <li class="list-group-item">Прізвище: {{$auth_user->surname}}</li>
-                            @if($auth_user->show_phone)
-                                <li class="list-group-item">Телефон: {{$auth_user->phone}}</li>
-                            @endif
-                            <li class="list-group-item">Стать: 
-                                @if ($auth_user->sex === 'Male')
-                                    Чоловік
-                                @elseif ($auth_user->sex === 'Female')
-                                    Жінка
-                                @else
-                                    Undefined
-                                @endif
-                            </li>
-                            {{--  <li class="list-group-item">Показати телефон: {{$auth_user->show_phone}}</li>  --}}
-                          </ul>
+                    <div class="text-center mb-4">
+                        <img src="{{$auth_user->avatar}}" alt="Avatar" class="rounded-circle" style="width:120px;height:120px;">
                     </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Нікнейм: {{$auth_user->nickname}}</li>
+                        <li class="list-group-item">Прізвище: {{$auth_user->surname}}</li>
+                        @if($auth_user->show_phone)
+                            <li class="list-group-item">Телефон: {{$auth_user->phone}}</li>
+                        @endif
+                        <li class="list-group-item">Стать:
+                            @if ($auth_user->sex === 'Male')
+                                Чоловік
+                            @elseif ($auth_user->sex === 'Female')
+                                Жінка
+                            @else
+                                Undefined
+                            @endif
+                        </li>
+                    </ul>
                     @if(session()->get('success'))
                         <div class="alert alert-success mt-2">
                             {{ session()->get('success') }}
