7ee232a86ea6deb662716aa98105014feaf42d80
Sergey <neroda.sergey1998@gmail.com>
2025-06-06 01:07:07 +0300

    Tie homepage counters to database metrics

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -17,12 +18,35 @@ class HomeController extends Controller
                 ->orderBy('created_at', 'desc')
                 ->take(6)
                 ->get();
-            return view('home', ['articles' => $articles, 'error' => '']);
+
+            $resourcesCount = Article::count();
+            $usersCount = User::count();
+            $downloadsCount = 0;
+
+            return view('home', [
+                'articles' => $articles,
+                'error' => '',
+                'resourcesCount' => $resourcesCount,
+                'downloadsCount' => $downloadsCount,
+                'usersCount' => $usersCount,
+            ]);
         } catch (\Exception $exception) {
             if($exception instanceof \Illuminate\Database\QueryException) {
-                return view('home', ['articles'=>[], 'error'=>'Помилка в базі данних. Неможливо відобразити статті']);
-            } 
-            return view('home', ['articles'=>$articles, 'error'=>'Помилка показу статтей']);
-        }  
+                return view('home', [
+                    'articles'=>[],
+                    'error'=>'Помилка в базі данних. Неможливо відобразити статті',
+                    'resourcesCount' => 0,
+                    'downloadsCount' => 0,
+                    'usersCount' => 0,
+                ]);
+            }
+            return view('home', [
+                'articles'=>$articles ?? [],
+                'error'=>'Помилка показу статтей',
+                'resourcesCount' => 0,
+                'downloadsCount' => 0,
+                'usersCount' => 0,
+            ]);
+        }
     }
 }
